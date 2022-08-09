8338a5510ab7c8d2d5b7ec97d03078ea03d35786
Nitin <62949170+nitinkrthakur@users.noreply.github.com>
2022-08-09 12:56:34 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,21 +1,21 @@
 //theme light and dark mode
 
-const dayNight=document.querySelector(".day-night");
-dayNight.addEventListener("click",()=>{
-   
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    dayNight.querySelector("i").classList.toggle("fa-sun");
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+
+    dayNight.querySelector("i").classList.toggle("fa-cloud-moon");
+    dayNight.querySelector("i").classList.toggle("fa-cloud-sun");
     document.body.classList.toggle("light");
 
 })
-window.addEventListener("load",()=>{
-    if(document.body.classList.contains("light"))
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("light")) {
+        dayNight.querySelector("i").classList.add("fa-cloud-moon");
     }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");
+    else {
+        dayNight.querySelector("i").classList.add("fa-cloud-sun");
     }
-    
+
 })
+
+
